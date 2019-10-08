0207821b88ca46dc6e77dca70e19e74609a46ace
Lena P <lena.petenok@gmail.com>
2019-10-08 11:44:54 +0300

    CRUD bugs fixed, changed a way of working with local storage, added a way to sort locally stored todo list

--- src/index.js
+++ src/index.js
@@ -3,143 +3,14 @@ import './styles/style.sass';
 // onClick -> stop propagation
 // p.getBy.onClick = function (e) {e.stopPropagation(); do smth}
 
-// Sort all saved items by Done/Undone first and then by priority
 // Add possibility to search by any text in the To-Do item (by hiding items that do not match)
 
-function hideModalWindow() {
-  document.getElementById("modal-window").classList.add("hidden");
-  document.querySelector(`#modal-window .form-elements-block`).classList.add("hidden");
-  document.getElementById("create-item-window").classList.add("hidden");
-  document.getElementById("edit-item-window").classList.add("hidden");
+function getTodoListFromLocalStorage() {
+  return JSON.parse(window.localStorage.getItem("toDoListData"));
 }
 
-function showModalWindowToCreateItem() {
-  document.querySelector(`#modal-window .form-elements-block`).classList.remove("hidden");
-  document.getElementById("create-item-window").classList.remove("hidden");
-  document.getElementById("modal-window").classList.remove("hidden");
-}
-
-function showModalWindowToEditItem(data) {
-  let name = document.getElementById("name");
-  let description = document.getElementById("description");
-  let priority = document.getElementById("priority");
-  let deadline = document.getElementById("deadline");
-  let title = document.querySelector(`#modal-window .modal-title`);
-
-  name.value = data.childNodes[0].innerText;
-  if ("No description" != data.childNodes[1].innerText) {
-    description.value = data.childNodes[1].innerText;
-  }
-  priority.value = data.childNodes[2].innerText.split(' ')[1];
-  if ("No deadline" != data.childNodes[3].innerText) {
-    deadline.value = data.childNodes[3].innerText;
-  }
-  title.innerText = "Edited item: " + data.id;
-
-  document.querySelector(`#modal-window .form-elements-block`).classList.remove("hidden");
-  document.getElementById("edit-item-window").classList.remove("hidden");
-  document.getElementById("modal-window").classList.remove("hidden");
-}
-
-function onItemEditButtonClicked(event) {
-  let data = event.target.parentElement.parentElement;
-  if (data.childNodes[4].innerText == "DONE") { alert("Already done"); }
-  else { showModalWindowToEditItem(data); }
-}
-
-function setFormToDefault() {
-  let name = document.getElementById("name");
-  let description = document.getElementById("description");
-  let priority = document.getElementById("priority");
-  let deadline = document.getElementById("deadline");
-  let title = document.querySelector(`#modal-window .modal-title`);
-  name.value = "";
-  description.value = "";
-  priority.value = "";
-  deadline.value = "";
-  title.innerText = "Create new item";
-}
-
-function collectFormDataForEditing() {
-  const name = document.getElementById("name").value;
-  const description = document.getElementById("description").value;
-  const priority = document.getElementById("priority").value;
-  const deadline = document.getElementById("deadline").value;
-  const id = parseInt(document.querySelector(`#modal-window .modal-title`).innerText.split(' ')[2]);
-
-  return {
-    id: id,
-    name: name ? name : '',
-    description: description ? description : '',
-    priority: priority ? priority : '',
-    deadline: deadline ? deadline : '',
-    isDone: false
-  };
-}
-
-function onModalWindowEditButtonClicked() {
-  const data = collectFormDataForEditing();
-
-  let currentList = JSON.parse(locallyStoredData.getItem("toDoListData"));
-  for (let item of currentList) {
-    if (item.id == data.id) {
-      if (data.description) { item.description = data.description; }
-      else { item.description = "No description"; }
-
-      if (data.deadline) { item.deadline = data.deadline; }
-      else { item.deadline = "No deadline"; }
-
-      item.name = data.name;
-      item.priority = data.priority;
-      item.isDone = false;
-      break;
-    }
-  }
-  locallyStoredData.setItem("toDoListData", JSON.stringify(currentList));
-
-  const edited = document.querySelector(`.to-do-items-list .grid`).childNodes[data.id];
-  edited.childNodes[0].innerText = data.name;
-  if (data.description && (data.description != "No description")) { edited.childNodes[1].innerText = data.description; }
-  if (data.deadline && (data.deadline != "No deadline")) { edited.childNodes[3].innerText = data.deadline; }
-  edited.childNodes[4].innerText = "TO DO";
-  edited.childNodes[2].innerText = "Priority: ";
-  let span = document.createElement("span");
-  span.classList.add(`priority-` + data.priority);
-  span.innerText = data.priority;
-  edited.childNodes[2].append(span);
-
-  setFormToDefault();
-  hideModalWindow();
-}
-
-function onItemDeleteButtonClicked(event) {
-  const data = event.target.parentElement.parentElement;
-  let splice = 0;
-
-  let parent = data.parentElement;
-  for (let item of parent.children) {
-    if (item.id == data.id) {
-      parent.removeChild(item);
-      break;
-    }
-    splice++;
-  }
-
-  let currentList = JSON.parse(locallyStoredData.getItem("toDoListData"));
-  currentList.splice(splice, 1);
-  locallyStoredData.setItem("toDoListData", JSON.stringify(currentList));
-}
-
-function onItemMarkAsReadButtonClicked(event) {
-  const data = event.target.parentElement.parentElement;
-
-  let currentList = JSON.parse(locallyStoredData.getItem("toDoListData"));
-  for (let item of currentList) {
-    if (item.id == data.id) { item.isDone = true; break; }
-  }
-  locallyStoredData.setItem("toDoListData", JSON.stringify(currentList));
-
-  data.childNodes[4].innerText = "DONE";
+function setTodoListToLocalStorage(data) {
+  window.localStorage.setItem("toDoListData", JSON.stringify(data));
 }
 
 function renderToDoItem(data) {
@@ -208,10 +79,158 @@ function renderToDoItem(data) {
 }
 
 function renderToDoList() {
-  const currentList = JSON.parse(locallyStoredData.getItem("toDoListData"));
+  const currentList = getTodoListFromLocalStorage();
   for (let item in currentList) {
     renderToDoItem(currentList[item]);
   }
+}
+
+function hideModalWindow() {
+  document.getElementById("modal-window").classList.add("hidden");
+  document.querySelector(`#modal-window .form-elements-block`).classList.add("hidden");
+  document.getElementById("create-item-window").classList.add("hidden");
+  document.getElementById("edit-item-window").classList.add("hidden");
+}
+
+function showModalWindowToCreateItem() {
+  document.querySelector(`#modal-window .form-elements-block`).classList.remove("hidden");
+  document.getElementById("create-item-window").classList.remove("hidden");
+  document.getElementById("modal-window").classList.remove("hidden");
+}
+
+function showModalWindowToEditItem(data) {
+  let name = document.getElementById("name");
+  let description = document.getElementById("description");
+  let priority = document.getElementById("priority");
+  let deadline = document.getElementById("deadline");
+  let title = document.querySelector(`#modal-window .modal-title`);
+
+  name.value = data.childNodes[0].innerText;
+  if ("No description" != data.childNodes[1].innerText) {
+    description.value = data.childNodes[1].innerText;
+  }
+  priority.value = data.childNodes[2].innerText.split(' ')[1];
+  if ("No deadline" != data.childNodes[3].innerText) {
+    deadline.value = data.childNodes[3].innerText;
+  }
+  title.innerText = "Edited item: " + data.id;
+
+  document.querySelector(`#modal-window .form-elements-block`).classList.remove("hidden");
+  document.getElementById("edit-item-window").classList.remove("hidden");
+  document.getElementById("modal-window").classList.remove("hidden");
+}
+
+function onItemEditButtonClicked(event) {
+  let data = event.target.parentElement.parentElement;
+  if (data.childNodes[4].innerText == "DONE") { alert("Already done"); }
+  else { showModalWindowToEditItem(data); }
+}
+
+function setFormToDefault() {
+  let name = document.getElementById("name");
+  let description = document.getElementById("description");
+  let priority = document.getElementById("priority");
+  let deadline = document.getElementById("deadline");
+  let title = document.querySelector(`#modal-window .modal-title`);
+  name.value = "";
+  description.value = "";
+  priority.value = "";
+  deadline.value = "";
+  title.innerText = "Create new item";
+}
+
+function collectFormDataForEditing() {
+  const name = document.getElementById("name").value;
+  const description = document.getElementById("description").value;
+  const priority = document.getElementById("priority").value;
+  const deadline = document.getElementById("deadline").value;
+  const id = parseInt(document.querySelector(`#modal-window .modal-title`).innerText.split(' ')[2]);
+
+  return {
+    id: id,
+    name: name ? name : false,
+    description: description ? description : false,
+    priority: priority ? priority : false,
+    deadline: deadline ? deadline : false
+  };
+}
+
+function rerenderToDoItem(actualId, data) {
+  const edited = document.querySelector(`.to-do-items-list .grid`).childNodes[actualId +1];
+
+  edited.childNodes[0].innerText = data.name;
+  if (data.description && (data.description != "No description")) { edited.childNodes[1].innerText = data.description; }
+  if (data.deadline && (data.deadline != "No deadline")) { edited.childNodes[3].innerText = data.deadline; }
+  edited.childNodes[4].innerText = "TO DO";
+  edited.childNodes[2].innerText = "Priority: ";
+
+  let span = document.createElement("span");
+  span.classList.add(`priority-` + data.priority);
+  span.innerText = data.priority;
+  edited.childNodes[2].append(span);
+}
+
+function editItemInLocalStorage(data) {
+  let currentList = getTodoListFromLocalStorage();
+  let actualId = 0;
+
+  for (let item of currentList) {
+    if (item.id == data.id) {
+      if (data.description) { item.description = data.description; }
+      if (data.deadline) { item.deadline = data.deadline; }
+      if (data.name) { item.name = data.name; }
+      if (data.priority) { item.priority = data.priority; }
+      break;
+    }
+    actualId++;
+  }
+  setTodoListToLocalStorage(currentList);
+
+  return actualId;
+}
+
+function onModalWindowEditButtonClicked() {
+  const data = collectFormDataForEditing();
+  setFormToDefault();
+  const actualId = editItemInLocalStorage(data);
+  rerenderToDoItem(actualId, data);
+  hideModalWindow();
+}
+
+function deleteItemInLocalStorage(data) {
+  let splice = 0;
+  let parent = data.parentElement;
+
+  for (let item of parent.children) {
+    if (item.id == data.id) {
+      parent.removeChild(item);
+      break;
+    }
+    splice++;
+  }
+
+  return splice;
+}
+
+function onItemDeleteButtonClicked(event) {
+  const data = event.target.parentElement.parentElement;
+  let splice = deleteItemInLocalStorage(data);
+
+  let currentList = getTodoListFromLocalStorage();
+  currentList.splice(splice, 1);
+  setTodoListToLocalStorage(currentList);
+}
+
+function onItemMarkAsReadButtonClicked(event) {
+  const data = event.target.parentElement.parentElement;
+
+  let currentList = getTodoListFromLocalStorage();
+  for (let item of currentList) {
+    if (item.id == data.id) { item.isDone = true; break; }
+  }
+  setTodoListToLocalStorage(currentList);
+
+  data.childNodes[4].innerText = "DONE";
 }
 
 function collectFormData() {
@@ -224,27 +243,57 @@ function collectFormData() {
 
   return {
     id: id,
-    name: name ? name : '',
-    description: description ? description : '',
-    priority: priority ? priority : '',
-    deadline: deadline ? deadline : '',
+    name: name ? name : "Nameless",
+    description: description ? description : "No description",
+    priority: priority ? priority : "No priority",
+    deadline: deadline ? deadline : "No deadline",
     isDone: false
   };
 }
 
 function onSubmitClicked(event) {
+  // check validity, then just collect data
   const data = collectFormData();
   hideModalWindow();
-  let currentList = JSON.parse(locallyStoredData.getItem("toDoListData"));
+  let currentList = getTodoListFromLocalStorage();
   currentList.push(data);
-  locallyStoredData.setItem("toDoListData", JSON.stringify(currentList));
+  setTodoListToLocalStorage(currentList);
   renderToDoItem(data);
   setFormToDefault();
   event.preventDefault();
 }
 
+// currentList = sortByIsDone(currentList);
+function sortByIsDone(data) {
+  return data.sort ((a, b) => { return (a.isDone === b.isDone) ? 0 : a.isDone ? -1 : 1; });
+}
+
+function sortByPriority(data) {
+  return data.sort ((a, b) => {
+    if (a.priority === "high") { return -1; }
+
+    else if (a.priority === "middle") {
+      if (b.priority === "high") { return 1; }
+      else if (b.priority === "middle") { return 0; }
+      else if (b.priority === "low") { return -1; }
+    }
+
+    else if (a.priority === "low") { return 1; }
+  });
+}
+
+function sortLocalStorage() {
+  let currentList = getTodoListFromLocalStorage();
+  let donePrioritisedList = sortByPriority(currentList.filter(item => item.isDone === true));
+  let todoPrioritisedList = sortByPriority(currentList.filter(item => item.isDone === false));
+  let sortedList = donePrioritisedList.concat(todoPrioritisedList);
+  setTodoListToLocalStorage(sortedList);
+}
+
+// Sort all saved items by Done/Undone first and then by priority
 function onSortButtonClicked() {
-  console.log("sort");
+  sortLocalStorage();
+  // change dom
 }
 
 // control panel
@@ -271,9 +320,8 @@ const form = document.getElementById("form");
 form.addEventListener('submit', onSubmitClicked);
 
 // local storage
-let locallyStoredData = window.localStorage;
-// locallyStoredData.clear();
-if (locallyStoredData.getItem("toDoListData") === null) {
+window.localStorage.clear();
+if (window.localStorage.getItem("toDoListData") === null) {
   const defaultData = [{
     id: 1,
     name: "Create stuff",
@@ -287,7 +335,7 @@ if (locallyStoredData.getItem("toDoListData") === null) {
     description: "Description of the stuff",
     priority: "middle",
     deadline: "2019-10-17T07:08",
-    isDone: false
+    isDone: true
   },{
     id: 3,
     name: "Create",
@@ -295,10 +343,24 @@ if (locallyStoredData.getItem("toDoListData") === null) {
     priority: "low",
     deadline: "2020-10-17T07:08",
     isDone: false
+  },{
+    id: 4,
+    name: "Create",
+    description: "Description of the stuff",
+    priority: "middle",
+    deadline: "2019-10-17T07:08",
+    isDone: true
+  },{
+    id: 5,
+    name: "Create",
+    description: "Description of the stuff",
+    priority: "low",
+    deadline: "2020-10-17T07:08",
+    isDone: false
   }];
 
-  locallyStoredData.setItem("toDoListData", JSON.stringify(defaultData));
-  locallyStoredData.setItem("id", "4");
+  window.localStorage.setItem("toDoListData", JSON.stringify(defaultData));
+  window.localStorage.setItem("id", "6");
 }
 
 // first render of the list
